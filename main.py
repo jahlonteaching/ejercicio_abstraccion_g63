e4717b967a9e4c09d425c697ba97277ec595e919
Jpenuela420 <penuelaherrerajuanjose@gmail.com>
2024-04-25 21:54:26 -0500

    3 transformadores

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ class Transformador:
         self.tipo: int = tipo
 
 
-class Transformador_1(Transformador):
-    def metodo_morse(self):
-        codigo_morse = {
-            'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.',
-            'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.',
-            'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-',
-            'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..'
-        }
-
-        morse = " ".join([codigo_morse.get(letra.lower(), " ") for letra in self.s])
-        return morse
-
-
-class Transformador_2(Transformador):
-    pass
+class MorseConverter(Transformador):
+    def convert_to_morse(self):
+        morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                      'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                      'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                      'Y': '-.--', 'Z': '--..'}
+        morse_text = []
+        for i in self.s.upper():
+            if i in morse_code:
+                morse_text.append(morse_code[i])
+            elif i == ' ':
+                morse_text.append(' ')
+        return ' '.join(morse_text)
 
 
-class Transformador_3(Transformador):
-    pass
+class BinaryConverter(Transformador):
+    def convert_to_binary(self):
+        binary_text = ' '.join(format(ord(i), '08b') for i in self.s)
+        return binary_text
+
+
+class LetterToNumberConverter(Transformador):
+    def convert_to_numbers(self):
+        numbers_text = []
+        for i in self.s:
+            if i.isalpha():
+                numbers_text.append(str(ord(i) - 64))
+            else:
+                numbers_text.append(i)
+        return ' '.join(numbers_text)
